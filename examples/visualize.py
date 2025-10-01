3e354a610403e765116f88948ec0be825a1b685a
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-01 10:27:45 -0600

    docs: generate visualizations of multiple seeds

--- examples/visualize.py
+++ examples/visualize.py
@@ -10,43 +10,44 @@ from foragax.registry import make
 
 def main():
     """Generate a visualization of a Foragax environment under random behavior."""
-    key = jax.random.key(0)
-    aperture_sizes = [3, 5, 7, 9, 11, 13, 15]
-    render_modes = ["world", "aperture"]
-    video_folder = "videos"
-    if not os.path.exists(video_folder):
-        os.makedirs(video_folder)
+    for seed in range(5):
+        key = jax.random.key(seed)
+        aperture_sizes = [3, 5, 7, 9, 11, 13, 15]
+        render_modes = ["world", "aperture"]
+        video_folder = "videos"
+        if not os.path.exists(video_folder):
+            os.makedirs(video_folder)
 
-    for aperture_size in aperture_sizes:
-        env = make(
-            "ForagaxWeather-v3",
-            aperture_size=aperture_size,
-            observation_type="object",
-        )
-        env_params = env.default_params
+        for aperture_size in aperture_sizes:
+            env = make(
+                "ForagaxTwoBiome-v9",
+                aperture_size=aperture_size,
+                observation_type="object",
+            )
+            env_params = env.default_params
 
-        frames = defaultdict(list)
-        key, key_reset = jax.random.split(key)
-        _, env_state = env.reset(key_reset, env_params)
-        for _ in tqdm(range(100), desc=f"Aperture {aperture_size}"):
-            for render_mode in render_modes:
-                frames[render_mode].append(
-                    env.render(env_state, env_params, render_mode=render_mode)
+            frames = defaultdict(list)
+            key, key_reset = jax.random.split(key)
+            _, env_state = env.reset_env(key_reset, env_params)
+            for _ in tqdm(range(100), desc=f"Aperture {aperture_size}"):
+                for render_mode in render_modes:
+                    frames[render_mode].append(
+                        env.render(env_state, env_params, render_mode=render_mode)
+                    )
+                key, key_act, key_step = jax.random.split(key, 3)
+                action = env.action_space(env_params).sample(key_act)
+                _, next_env_state, reward, done, info = env.step(
+                    key_step, env_state, action, env_params
                 )
-            key, key_act, key_step = jax.random.split(key, 3)
-            action = env.action_space(env_params).sample(key_act)
-            _, next_env_state, reward, done, info = env.step(
-                key_step, env_state, action, env_params
-            )
-            env_state = next_env_state
+                env_state = next_env_state
 
-        for render_mode in render_modes:
-            save_video(
-                frames[render_mode],
-                video_folder,
-                name_prefix=f"foragax_{render_mode}_test-aperture-{aperture_size}",
-                fps=8,
-            )
+            for render_mode in render_modes:
+                save_video(
+                    frames[render_mode],
+                    video_folder,
+                    name_prefix=f"foragax_{seed}_{render_mode}_test-aperture-{aperture_size}",
+                    fps=8,
+                )
 
 
 if __name__ == "__main__":
