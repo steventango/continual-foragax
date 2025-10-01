77c68f492e3f1a37818bbdec0b28dd25ce0c927a
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-01 11:36:57 -0600

    feat: ForagaxTwoBiome-v11 with higher mushroom freqs

--- src/foragax/registry.py
+++ src/foragax/registry.py
@@ -193,6 +193,19 @@ ENV_CONFIGS: Dict[str, Dict[str, Any]] = {
         "nowrap": True,
         "deterministic_spawn": True,
     },
+    "ForagaxTwoBiome-v11": {
+        "size": None,
+        "aperture_size": None,
+        "objects": (
+            BROWN_MOREL_UNIFORM,
+            BROWN_OYSTER_UNIFORM,
+            GREEN_DEATHCAP_UNIFORM,
+            GREEN_FAKE_UNIFORM,
+        ),
+        "biomes": None,
+        "nowrap": True,
+        "deterministic_spawn": True,
+    },
     "ForagaxTwoBiomeSmall-v1": {
         "size": (16, 8),
         "aperture_size": None,
@@ -284,6 +297,25 @@ def make(
                 start=(margin + 7, 0),
                 stop=(margin + 9, 15),
                 object_frequencies=(0.0, 0.25, 0.0, 0.5),
+            ),
+        )
+
+    if env_id == "ForagaxTwoBiome-v11":
+        margin = aperture_size[1] // 2 + 1
+        width = 2 * margin + 9
+        config["size"] = (width, 15)
+        config["biomes"] = (
+            # Morel biome
+            Biome(
+                start=(margin, 0),
+                stop=(margin + 2, 15),
+                object_frequencies=(0.5, 0.0, 0.25, 0.0),
+            ),
+            # Oyster biome
+            Biome(
+                start=(margin + 7, 0),
+                stop=(margin + 9, 15),
+                object_frequencies=(0.0, 0.5, 0.0, 0.25),
             ),
         )
 
