58ce00b1d6012718462a30b1af21221c93e9ca40
Steven Tang <18170455+steventango@users.noreply.github.com>
2025-10-09 15:55:13 -0600

    feat: remove ForagaxWeather-v6

--- src/foragax/registry.py
+++ src/foragax/registry.py
@@ -83,19 +83,6 @@ ENV_CONFIGS: Dict[str, Dict[str, Any]] = {
         "nowrap": False,
         "deterministic_spawn": True,
     },
-    "ForagaxWeather-v6": {
-        "size": (15, 15),
-        "aperture_size": None,
-        "objects": None,
-        "biomes": (
-            # Hot biome
-            Biome(start=(0, 3), stop=(15, 5), object_frequencies=(0.5, 0.0)),
-            # Cold biome
-            Biome(start=(0, 10), stop=(15, 12), object_frequencies=(0.0, 0.5)),
-        ),
-        "nowrap": False,
-        "deterministic_spawn": True,
-    },
     "ForagaxTwoBiome-v1": {
         "size": (15, 15),
         "aperture_size": None,
@@ -471,12 +458,10 @@ def make(
             "ForagaxWeather-v3",
             "ForagaxWeather-v4",
             "ForagaxWeather-v5",
-            "ForagaxWeather-v6",
         )
         random_respawn = env_id in (
             "ForagaxWeather-v4",
             "ForagaxWeather-v5",
-            "ForagaxWeather-v6",
         )
         hot, cold = create_weather_objects(
             file_index=file_index,
